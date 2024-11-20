9eba2f43d375069a645768b9ae5bc96e36fc612b
tessarivas <rivas.gomez.teresa@gmail.com>
2024-11-19 22:01:36 -0800

    pagina incio avance

--- src/pages/IdeasPage.jsx
+++ src/pages/IdeasPage.jsx
@@ -1,11 +1,54 @@
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from '../assets/photos/pantalon.png';
 import PageTitle from '../components/extras/PageTitle';
 
 const IdeasPage = () => {
     return (
-        <>
-            {/* Botón de Iniciar Sesión y Registrarme */}
-            <PageTitle>Ideas</PageTitle>
-        </>
+        <div>
+            {/* TITULO */}
+            <div>
+                <PageTitle>Ideas</PageTitle>
+            </div>
+            
+            {/* CARRUSEL DE IDEAS Y SUGERENCIAS */}
+            <div>
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                            src={ExampleCarouselImage}
+                            alt="First slide"
+                            className="d-block w-100"
+                        />
+                        <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            src={ExampleCarouselImage}
+                            alt="Second slide"
+                            className="d-block w-100"
+                        />
+                        <Carousel.Caption>
+                            <h3>Second slide label</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            src={ExampleCarouselImage}
+                            alt="Third slide"
+                            className="d-block w-100"
+                        />
+                        <Carousel.Caption>
+                            <h3>Third slide label</h3>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </div>
     );
 };
 
