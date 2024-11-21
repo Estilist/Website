df6cea8b3f209d65823d69e8944ac9d219eb2d98
Fernando Haro <fernando@haroware.com>
2024-11-20 17:56:50 -0800

    HU 2.2

--- src/components/extras/FeedbackModal.jsx
+++ src/components/extras/FeedbackModal.jsx
@@ -1,46 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Modal } from "react-bootstrap";
 import PropTypes from 'prop-types';
-import starFill from "../../assets/icons/star-fill.svg"; 
-import starEmpty from "../../assets/icons/star.svg";  
 import PrimaryButton from "../buttons/PrimaryButton";
+import StarRating from "../StarRating";
 
-const FeedbackModal = ({ show, onHide, }) => { 
+const FeedbackModal = ({ show, onHide }) => { 
     const [filledStars, setFilledStars] = useState(0);
 
-    const handleStarClick = (index) => {
-        if (index < filledStars) {
-            setFilledStars(index);
-        } else {
-            setFilledStars(index + 1);
+    useEffect(() => {
+        if (!show) {
+            setFilledStars(0);
         }
-    };
+    }, [show]);
+
     return (
         <Modal
             show={show}
             onHide={onHide}
             size="lg"
-            aria-labelledby = "contained-modal-title-vcenter"
+            aria-labelledby="contained-modal-title-vcenter"
             centered
-            dialogClassName = "custom-modal"
+            dialogClassName="custom-modal"
         >
             <Modal.Header closeButton />
             <Modal.Body>
-                <p className="feedback-text"> ¿Cómo te están</p>
-                <p className="feedback-text"> pareciendo las</p>
-                <p className="feedback-text"> recomendaciones</p>
-                <p className="feedback-text"> de la app?</p>
-                <div className="feedback-stars">
-                    {[...Array(5)].map((_, index) => (
-                        <img
-                            key={index}
-                            src={index < filledStars ? starFill : starEmpty}
-                            alt={`Estrella ${index + 1}`}
-                            className="feedback-star-icon"
-                            onClick={() => handleStarClick(index)}
-                        />
-                    ))}
-                </div>
+                <p className="feedback-text"> ¿Cómo te están pareciendo las recomendaciones de la app</p>
+                <StarRating starRating={filledStars} setStarRating={setFilledStars} />
                 <p className="feedback-thanks">
                     ¡Gracias!
                 </p>
