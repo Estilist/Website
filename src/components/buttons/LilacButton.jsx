5ed7af0d255552b7f17da62cca1b7a8e8aa5462c
tessarivas <rivas.gomez.teresa@gmail.com>
2024-11-20 21:24:23 -0800

    H.U 2.3: Personalizar recomendación por temporada

--- src/components/buttons/LilacButton.jsx
+++ src/components/buttons/LilacButton.jsx
@@ -11,7 +11,8 @@ const LilacButton = ({ size, onClick, children, className }) => {
         fontSize: '21px',
         boxShadow: '0 4px 4px rgba(0, 0, 0, 0.25)',
         borderRadius: '18px',
-        width: '280px',
+        width: '100%',
+        maxWidth: '280px',
         textAlign: 'center',
     };
 
